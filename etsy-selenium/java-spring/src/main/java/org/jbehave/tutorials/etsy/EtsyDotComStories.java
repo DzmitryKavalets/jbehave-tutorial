0767466c511b3630ef2f9939695f1a8cacee6b18
dkavalets <dzmitry_kavalets@epam.com>
2016-11-29 09:27:45 +0300

    Add integration with reportportal

--- etsy-selenium/java-spring/src/main/java/org/jbehave/tutorials/etsy/EtsyDotComStories.java
+++ etsy-selenium/java-spring/src/main/java/org/jbehave/tutorials/etsy/EtsyDotComStories.java
@@ -1,5 +1,10 @@
 package org.jbehave.tutorials.etsy;
 
+import static java.util.Arrays.asList;
+import static org.jbehave.core.io.CodeLocations.codeLocationFromClass;
+import static org.jbehave.core.reporters.Format.CONSOLE;
+import static org.jbehave.web.selenium.WebDriverHtmlOutput.WEB_DRIVER_HTML;
+
 import java.util.List;
 
 import org.jbehave.core.configuration.Configuration;
@@ -8,60 +13,49 @@ import org.jbehave.core.failures.FailingUponPendingStep;
 import org.jbehave.core.failures.PendingStepStrategy;
 import org.jbehave.core.io.LoadFromClasspath;
 import org.jbehave.core.io.StoryFinder;
-import org.jbehave.core.junit.JUnitStories;
 import org.jbehave.core.reporters.CrossReference;
 import org.jbehave.core.reporters.Format;
 import org.jbehave.core.reporters.StoryReporterBuilder;
 import org.jbehave.core.steps.InjectableStepsFactory;
 import org.jbehave.core.steps.spring.SpringApplicationContextFactory;
 import org.jbehave.core.steps.spring.SpringStepsFactory;
-import org.jbehave.web.selenium.ContextView;
-import org.jbehave.web.selenium.LocalFrameContextView;
-import org.jbehave.web.selenium.SeleniumConfiguration;
-import org.jbehave.web.selenium.SeleniumContext;
-import org.jbehave.web.selenium.SeleniumContextOutput;
-import org.jbehave.web.selenium.SeleniumStepMonitor;
+import org.jbehave.web.selenium.*;
 import org.springframework.context.ApplicationContext;
 
-import static java.util.Arrays.asList;
-import static org.jbehave.core.io.CodeLocations.codeLocationFromClass;
-import static org.jbehave.core.reporters.Format.CONSOLE;
-import static org.jbehave.web.selenium.WebDriverHtmlOutput.WEB_DRIVER_HTML;
+import com.epam.reportportal.jbehave.ReportPortalFormat;
+import com.epam.reportportal.jbehave.RpJUnitStories;
 
-public class EtsyDotComStories extends JUnitStories {
+public class EtsyDotComStories extends RpJUnitStories {
 
-    PendingStepStrategy pendingStepStrategy = new FailingUponPendingStep();
-    CrossReference crossReference = new CrossReference().withJsonOnly().withPendingStepStrategy(pendingStepStrategy)
-            .withOutputAfterEachStory(true).excludingStoriesWithNoExecutedScenarios(true);
-    ContextView contextView = new LocalFrameContextView().sized(640, 120);
-    SeleniumContext seleniumContext = new SeleniumContext();
-    SeleniumStepMonitor stepMonitor = new SeleniumStepMonitor(contextView, seleniumContext,
-            crossReference.getStepMonitor());
-    Format[] formats = new Format[] { new SeleniumContextOutput(seleniumContext), CONSOLE, WEB_DRIVER_HTML };
-    StoryReporterBuilder reporterBuilder = new StoryReporterBuilder()
-            .withCodeLocation(codeLocationFromClass(EtsyDotComStories.class)).withFailureTrace(true)
-            .withFailureTraceCompression(true).withDefaultFormats().withFormats(formats)
-            .withCrossReference(crossReference);
+	PendingStepStrategy pendingStepStrategy = new FailingUponPendingStep();
+	CrossReference crossReference = new CrossReference().withJsonOnly().withPendingStepStrategy(pendingStepStrategy)
+			.withOutputAfterEachStory(true).excludingStoriesWithNoExecutedScenarios(true);
+	ContextView contextView = new LocalFrameContextView().sized(640, 120);
+	SeleniumContext seleniumContext = new SeleniumContext();
+	SeleniumStepMonitor stepMonitor = new SeleniumStepMonitor(contextView, seleniumContext, crossReference.getStepMonitor());
+	Format[] formats = new Format[] { new SeleniumContextOutput(seleniumContext), CONSOLE, WEB_DRIVER_HTML, ReportPortalFormat.INSTANCE };
+	StoryReporterBuilder reporterBuilder = new StoryReporterBuilder().withCodeLocation(codeLocationFromClass(EtsyDotComStories.class))
+			.withFailureTrace(true).withFailureTraceCompression(true).withDefaultFormats().withFormats(formats)
+			.withCrossReference(crossReference);
 
-    @Override
-    public Configuration configuration() {
-        return new SeleniumConfiguration().useSeleniumContext(seleniumContext)
-                .usePendingStepStrategy(pendingStepStrategy)
-                .useStoryControls(new StoryControls().doResetStateBeforeScenario(false)).useStepMonitor(stepMonitor)
-                .useStoryLoader(new LoadFromClasspath(EtsyDotComStories.class))
-                .useStoryReporterBuilder(reporterBuilder);
-    }
+	@Override
+	public Configuration configuration() {
+		return new SeleniumConfiguration().useSeleniumContext(seleniumContext).usePendingStepStrategy(pendingStepStrategy)
+				.useStoryControls(new StoryControls().doResetStateBeforeScenario(false)).useStepMonitor(stepMonitor)
+				.useStoryLoader(new LoadFromClasspath(EtsyDotComStories.class))
+				.useStoryReporterBuilder(reporterBuilder);
+	}
 
-    @Override
-    public InjectableStepsFactory stepsFactory() {
-        ApplicationContext context = new SpringApplicationContextFactory("etsy-steps.xml").createApplicationContext();
-        return new SpringStepsFactory(configuration(), context);
-    }
+	@Override
+	public InjectableStepsFactory stepsFactory() {
+		ApplicationContext context = new SpringApplicationContextFactory("etsy-steps.xml").createApplicationContext();
+		return new SpringStepsFactory(configuration(), context);
+	}
 
-    @Override
-    protected List<String> storyPaths() {
-        return new StoryFinder().findPaths(codeLocationFromClass(this.getClass()).getFile(),
-                asList("**/" + System.getProperty("storyFilter", "*") + ".story"), null);
-    }
+	@Override
+	protected List<String> storyPaths() {
+		return new StoryFinder().findPaths(codeLocationFromClass(this.getClass()).getFile(),
+				asList("**/" + System.getProperty("storyFilter", "*") + ".story"), null);
+	}
 
 }
